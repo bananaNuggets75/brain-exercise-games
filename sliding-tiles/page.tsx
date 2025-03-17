7d335a8030cd663f543cb7cfc814e8bed4726701
Kenan Ben <gazokenan@gmail.com>
2025-03-17 23:21:37 +0800

    initial sliding tiles

---
+++ sliding-tiles/page.tsx
@@ -0,0 +1,8 @@
+export default function SlidingTilesPage() {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <h1 className="text-4xl font-bold">Sliding Tiles Game</h1>
+      </div>
+    );
+  }
+  
